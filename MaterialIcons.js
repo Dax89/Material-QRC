3fa75e1748e62c338a63bff4bf8cf0f055ae2ba4
Dax89 <trogu.davide@gmail.com>
2017-10-24 16:42:42 +0200

    Small fixes

--- MaterialIcons.js
+++ MaterialIcons.js
@@ -3,12 +3,12 @@
 var _theme = "black";
 
 function themeIcon(name, theme) {
-    return "qrc:///material-icons/" + color + "/" + theme + "/" + name + ".png";
+    return "qrc:///material-icons/" + "/" + theme + "/" + name + ".png";
 }
 
 function lightIcon(name) { return themeIcon(name, "black"); }
 function darkIcon(name) { return themeIcon(name, "light"); }
-function icon(name) { themeIcon(name, _theme); }
+function icon(name) { return themeIcon(name, _theme); }
 
 function setLightTheme() { _theme = "black"; }
 function setDarkTheme() { _theme = "white"; }
